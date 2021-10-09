6afdc4fba92be687995ce1292af131cbf6e8d7a6
NakraniKrupali0 <92012510+NakraniKrupali@users.noreply.github.com>
2021-10-09 16:12:14 +0000

    sd

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const os = require("os");
 
-console.log("\n Operating System Architecture :- " + os.arch());
+console.log("\n Operating System Architecture : " + os.arch());
 
-console.log("\n Free Memory Of The Operating System :- " + 
+console.log("\n Free Memory Of The Operating System : " + 
 os.freemem()/(1024*1024*1024));
 
-console.log("\n Total Memory Of The Operating system :- " + os.totalmem()/(1024*1024*1024));
+console.log("\n Total Memory Of The Operating system : " + os.totalmem()/(1024*1024*1024));
 
-console.log("\n Operating System Platform :-" + os.platform());
+console.log("\n Operating System Platform : " + os.platform());
 
-console.log("\n Display Information About The Current Looged User :-");
+console.log("\n Display Information About The Current Looged User : " +os.userInfo());
 
-console.log(os.userInfo());
+// console.log(os.userInfo());
